2f4a6e7188c105fa38256d5a1ba01ca8775d636c
sopiadi19 <sopiadi2006@gmail.com>
2024-03-03 10:36:12 +0700

    setup spatie

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,14 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('admin', function () {
+    return 'Hi admin';
+})->middleware('role:admin');
+
+Route::get('user', function () {
+    return 'Hi user';
+})->middleware('role:user');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
